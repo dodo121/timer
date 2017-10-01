37bc554017609b30f26262cdb488a7d162d4ccab
Dominik Szromik <dodox360@gmail.com>
2017-10-01 19:24:45 +0200

    change idea, new cities will be added in subscreen

--- components/world_clock.js
+++ components/world_clock.js
@@ -1,55 +1,31 @@
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 import {
   Text,
   View,
+  Button,
   TextInput,
   FlatList,
   StyleSheet
 } from 'react-native';
 
-export default class WorldClock extends Component {
-  static naviationOptions = {
+class CitiesList extends Component {
+  static navigationOptions = {
     title: 'World Clock'
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
-      showPrompt: false,
       itemsSelected: []
     }
-  }
-
-  showPrompt = () => {
-    this.setState({showPrompt: true});
-  };
-
-  hidePrompt = () => {
-    this.setState({showPrompt: false});
-  };
-
-  newItemSelected = (item) => {
-    items = this.state.itemsSelected;
-    items.push(item);
-    this.setState({itemsSelected: items});
   };
 
   render() {
-    prompt = null;
-    if(this.state.showPrompt) {
-      prompt = <Prompt newItemSelected={(item) => this.newItemSelected(item)}/>;
-    }
     const { navigate } = this.props.navigation;
     return(
       <View>
-        <TextInput
-          value={this.state.text}
-          placeholder={'Search...'}
-          onFocus={() => { this.showPrompt() }}
-          onBlur={() => { this.hidePrompt() }} />
-        {prompt}
+        <Button title='+' onPress={() => {navigate('AddNewCity')}} />
         <FlatList
           data={this.state.itemsSelected}
           renderItem={({item}) => <Text key={item} style={styles.selectedListItem}>{item}</Text>}
@@ -65,21 +41,68 @@ class Prompt extends Component {
     super(props);
   }
 
-  selected = (e) => {
-    this.props.newItemSelected(e);
-  };
-
   render() {
     return(
       <View>
         <FlatList
           data={['a', 'b']}
-          renderItem={({item}) => <Text key={item} onPress={() => { this.selected(item) }} style={styles.listItem}>{item}</Text>}
+          renderItem={({item}) => <Text key={item} onPress={() => {this.props.newItemSelected(item)}}
+          style={styles.listItem}>{item}</Text>}
         />
       </View>
     )
   }
 }
+
+class AddNewCity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      showPrompt: false
+    }
+  }
+
+  newItemSelected = (item) => {
+//    items = this.state.itemsSelected;
+//    items.push(item);
+//    this.setState({itemsSelected: items});
+    AsyncStorage.setItem('backgroundColor', color.toString());
+    this.props.navigation.navigate('CitiesList');
+  };
+
+  showPrompt = () => {
+    this.setState({showPrompt: true});
+  };
+
+  hidePrompt = () => {
+    this.setState({showPrompt: false});
+  };
+
+  render() {
+    prompt = null;
+    if(this.state.showPrompt) {
+      prompt = <Prompt newItemSelected={(item) => this.newItemSelected(item)}/>;
+    }
+    const { navigate } = this.props.navigation;
+    return(
+      <View>
+        <TextInput
+          value={this.state.text}
+          placeholder={'Search...'}
+          onFocus={() => { this.showPrompt() }}/>
+        {prompt}
+      </View>
+    )
+  }
+}
+
+const WorldClock = StackNavigator({
+  CitiesList: { screen: CitiesList },
+  AddNewCity: { screen: AddNewCity }
+});
+
+export default WorldClock;
 
 const styles = StyleSheet.create({
   listItem: {
